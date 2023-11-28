67a787199f15d743f9ee07e8809ac8b77fdaad54
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-11-29 00:18:51 +0100

    first commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    constructor(private signature: number = Math.random()) {
+        this.signature = signature;
+    }
+
+    getSignature() {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected tenants: Person[] = []
+    protected door: boolean = false;
+    
+    constructor(protected  key: Key) {}
+
+    comeIn(person: Person){
+        if(this.door) {
+            this.tenants.push(person)
+            console.log(`Welcome home, ${person.getKey().getSignature()}!`);
+        } else {
+            console.log("The door is closed.");
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+    
+    openDoor(key: Key): void {
+        if (key === this.key) {
+            this.door = true;
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
